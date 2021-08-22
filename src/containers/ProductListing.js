406fcac4526f5bc406b18a106825d88ac8c6d828
Suraj Kumar Das <59340117+SurajKD@users.noreply.github.com>
2021-08-22 18:19:56 +0530

    12

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -24,7 +24,7 @@ const ProductListing = () => {
 
   return (
     <div className="container section">
-      <div className="row row-cols-1 row-cols-md-4 g-4">
+      <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4   g-4">
         <ProductComponent />
       </div>
     </div>
